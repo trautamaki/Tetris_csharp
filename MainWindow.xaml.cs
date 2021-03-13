45272caa3804979f3dc5d7ff7a4f5434e4cb379c
rautamak <timi.rautamaki@gmail.com>
2021-03-13 18:44:02 +0200

    Draw a static tetromino

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System.Windows;
+﻿using System.Diagnostics;
+using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Shapes;
@@ -12,13 +13,32 @@ namespace Tetris_csharp
     public partial class MainWindow : Window
     {
         bool DEBUG = true;
+        struct tetromino_pos
+        {
+            public int x;
+            public int y;
+        };
+
+        // Whole game field, initialized with size COLUMNS*ROWS
+        // values:
+        //  0: empty
+        //  1: active tetromino
+        //  2..9: finished tetromino
         List<List<int>> field_;
+
+        // 4*4 that holds the information of each piece in the tetromino
+        List<List<tetromino_pos>> position_;
+
+        Tetromino current_;
+        
+        int current_shape_;
 
         public MainWindow()
         {
             InitializeComponent();
             Init();
             DrawGrid();
+            GameLoop();
         }
 
         void Init()
@@ -30,6 +50,125 @@ namespace Tetris_csharp
                 for (int y = 0; y < Constants.COLUMNS; ++y)
                 {
                     field_[x].Add(0);
+                }
+            }
+
+            position_ = new List<List<tetromino_pos>>();
+            for (int x = 0; x < 4; ++x)
+            {
+                position_.Add(new List<tetromino_pos>());
+                for (int y = 0; y < 4; ++y)
+                {
+                    position_[x].Add(new tetromino_pos { x = 0, y = 0 });
+                }
+            }
+        }
+
+        void GameLoop()
+        {
+            CreateBlock(0);
+            Draw();
+        }
+
+        void CreateBlock(int tetromino)
+        {
+            Debug.WriteLine("Create a shape");
+            int start_x = 4;
+            int start_y = 0;
+
+            current_shape_ = tetromino;
+            Tetromino t = new Tetromino(current_shape_);
+
+            switch (tetromino)
+            {
+                case (int) Constants.TETROMINO_KIND.HORIZONTAL:
+                    start_x = 4;
+                    start_y = 0;
+                    break;
+                case (int)Constants.TETROMINO_KIND.SQUARE:
+                    start_x = 4;
+                    start_y = 0;
+                    break;
+                case (int)Constants.TETROMINO_KIND.STEP_UP_RIGHT:
+                case (int)Constants.TETROMINO_KIND.STEP_UP_LEFT:
+                case (int)Constants.TETROMINO_KIND.LEFT_CORNER:
+                case (int)Constants.TETROMINO_KIND.RIGHT_CORNER:
+                case (int)Constants.TETROMINO_KIND.PYRAMID:
+                    start_x = 4;
+                    start_y = 0;
+                    break;
+            }
+
+            current_ = t;
+
+            for (int x = 0; x < 4; x++)
+            {
+                for (int y = 0; y < 4; y++)
+                {
+                    position_[x][y] = 
+                        new tetromino_pos { x = start_x + x, y = start_y + y };
+
+                    // To allow the 4*4 to go over the top
+                    if (start_y + y < 0) continue;
+
+                    field_[start_x + x][start_y + y] = current_.GetShape()[x][y];
+                }
+            }
+        }
+
+        void Draw()
+        {
+            for (int x = 0; x < Constants.ROWS; ++x)
+            {
+                for (int y = 0; y < Constants.COLUMNS; ++y)
+                {
+                    if (field_[x][y] == 0)
+                    {
+                        Debug.WriteLine("empty");
+                        // Skip empty spots
+                        continue;
+                    }
+                    else if (field_[x][y] == 1)
+                    {
+                        SolidColorBrush c = new SolidColorBrush(current_.GetColor());
+
+                        Rectangle r = new Rectangle();
+                        r.Stroke = c;
+                        r.Fill = c;
+                        r.StrokeThickness = 1;
+                        r.Width = Constants.SQUARE_SIDE;
+                        r.Height = Constants.SQUARE_SIDE;
+                        Canvas.SetLeft(r, y * Constants.SQUARE_SIDE);
+                        Canvas.SetTop(r, x * Constants.SQUARE_SIDE);
+
+                        mainCanvas.Children.Add(r);
+                    }
+                    else if (field_[x][y] >= 2)
+                    {
+                        SolidColorBrush c = new SolidColorBrush(current_.GetColor());
+
+                        Rectangle r = new Rectangle();
+                        r.Stroke = c;
+                        r.Fill = c;
+                        r.StrokeThickness = 1;
+                        r.Width = Constants.SQUARE_SIDE;
+                        r.Height = Constants.SQUARE_SIDE;
+                        Canvas.SetLeft(r, y * Constants.SQUARE_SIDE);
+                        Canvas.SetTop(r, x * Constants.SQUARE_SIDE);
+
+                        mainCanvas.Children.Add(r);
+                    }
+
+                    if (DEBUG)
+                    {
+                        TextBlock textBlock = new TextBlock();
+                        textBlock.Text = field_[x][y].ToString();
+                        textBlock.Foreground = new SolidColorBrush(Colors.Black);
+                        Canvas.SetLeft(textBlock, y * Constants.SQUARE_SIDE);
+                        Canvas.SetTop(textBlock, x * Constants.SQUARE_SIDE);
+
+                        mainCanvas.Children.Add(textBlock);
+                    }
                 }
             }
         }
@@ -51,16 +190,6 @@ namespace Tetris_csharp
                     Canvas.SetTop(r, x * Constants.SQUARE_SIDE);
 
                     mainCanvas.Children.Add(r);
-
-                    if (DEBUG)
-                    {
-                        TextBlock textBlock = new TextBlock();
-                        textBlock.Text = field_[x][y].ToString();
-                        textBlock.Foreground = new SolidColorBrush(Colors.Black);
-                        Canvas.SetLeft(textBlock, y * Constants.SQUARE_SIDE);
-                        Canvas.SetTop(textBlock, x * Constants.SQUARE_SIDE);
-                        mainCanvas.Children.Add(textBlock);
-                    }
                 }
             }
         }
---
+++ Tetromino.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Media;
+
+namespace Tetris_csharp
+{
+    class Tetromino
+    {
+        public int type;
+
+        public Tetromino(int t_type)
+        {
+            type = t_type;
+        }
+
+        public Color GetColor()
+        {
+            switch (type)
+            {
+                case (int)Constants.TETROMINO_KIND.HORIZONTAL:
+                    return Colors.Cyan;
+                case (int)Constants.TETROMINO_KIND.SQUARE:
+                    return Colors.Blue;
+                case (int)Constants.TETROMINO_KIND.STEP_UP_RIGHT:
+                    return Colors.Orange;
+                case (int)Constants.TETROMINO_KIND.STEP_UP_LEFT:
+                    return Colors.Yellow;
+                case (int)Constants.TETROMINO_KIND.LEFT_CORNER:
+                    return Colors.Green;
+                case (int)Constants.TETROMINO_KIND.RIGHT_CORNER:
+                    return Colors.Magenta;
+                case (int)Constants.TETROMINO_KIND.PYRAMID:
+                    return Colors.Red;
+                default:
+                    return Colors.Black;
+            }
+        }
+        
+        public List<List<int>> GetShape()
+        {
+            List<List<int>> shape = new List<List<int>>();
+
+            switch (type)
+            {
+                case (int)Constants.TETROMINO_KIND.HORIZONTAL:
+                    shape.Add(new List<int>() { 0, 0, 1, 0 });
+                    shape.Add(new List<int>() { 0, 0, 1, 0 });
+                    shape.Add(new List<int>() { 0, 0, 1, 0 });
+                    shape.Add(new List<int>() { 0, 0, 1, 0 });
+                    break;
+                case (int)Constants.TETROMINO_KIND.SQUARE:
+                    shape.Add(new List<int>() { 0, 0, 0, 0 }); 
+                    shape.Add(new List<int>() { 1, 1, 0, 0 });
+                    shape.Add(new List<int>() { 1, 1, 0, 0 });
+                    shape.Add(new List<int>() { 0, 0, 0, 0 });
+                    break;
+                case (int)Constants.TETROMINO_KIND.STEP_UP_RIGHT:
+                    shape.Add(new List<int>() { 0, 1, 0, 0 });
+                    shape.Add(new List<int>() { 0, 1, 1, 0 });
+                    shape.Add(new List<int>() { 0, 0, 1, 0 });
+                    shape.Add(new List<int>() { 0, 0, 0, 0 });
+                    break;
+                case (int)Constants.TETROMINO_KIND.STEP_UP_LEFT:
+                    shape.Add(new List<int>() { 0, 0, 1, 0 });
+                    shape.Add(new List<int>() { 0, 1, 1, 0 });
+                    shape.Add(new List<int>() { 0, 1, 0, 0 });
+                    shape.Add(new List<int>() { 0, 0, 0, 0 });
+                    break;
+                case (int)Constants.TETROMINO_KIND.LEFT_CORNER:
+                    shape.Add(new List<int>() { 0, 1, 1, 0 });
+                    shape.Add(new List<int>() { 0, 1, 0, 0 });
+                    shape.Add(new List<int>() { 0, 1, 0, 0 });
+                    shape.Add(new List<int>() { 0, 0, 0, 0 });
+                    break;
+                case (int)Constants.TETROMINO_KIND.RIGHT_CORNER:
+                    shape.Add(new List<int>() { 0, 1, 1, 0 });
+                    shape.Add(new List<int>() { 0, 0, 1, 0 });
+                    shape.Add(new List<int>() { 0, 0, 1, 0 });
+                    shape.Add(new List<int>() { 0, 0, 0, 0 });
+                    break;
+                case (int)Constants.TETROMINO_KIND.PYRAMID:
+                    shape.Add(new List<int>() { 0, 0, 1, 0 });
+                    shape.Add(new List<int>() { 0, 1, 1, 0 });
+                    shape.Add(new List<int>() { 0, 0, 1, 0 });
+                    shape.Add(new List<int>() { 0, 0, 0, 0 });
+                    break;
+            }
+
+            return shape;
+        }
+
+    }
+}
